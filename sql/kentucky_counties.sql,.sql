5a5d0295dd99a6b81c563bc98262877446ec9a49
avyma <56445958+avyma@users.noreply.github.com>
2019-12-14 22:03:36 -0500

    Created sql for the layers of the cholorpleth map

---
+++ sql/kentucky_counties.sql,.sql
@@ -0,0 +1,27 @@
+/* Step 1: Drop kentucky_counties if it already exists  in the database */
+drop tabel if exists
+		lab_10.kentucky_counties; -- end first statement
+		
+/* Step 2: Create kentucky_counties from the results of the query of cb_2018_us_county_20m */
+create table
+		lab_10.kentucky_counties -- schema_name.table_name
+as
+select
+		id,
+		countyfp,
+		geoid,
+		name,
+		lsad,
+		aland,
+		awater
+		
+from
+		lab_10.cb_2018_us_county_20m -- the source table
+		
+where
+		lab_10.cb_2018_us_state_20m.statefp=21; -- end second statement
+		
+/* Step 3: Third statement  to designate primary key */
+alter table
+		lab_10.kentucky_counties
+add primary key (id); -- end third statement
---
+++ sql/ky_mortality.sql,.sql
@@ -0,0 +1,32 @@
+/* Step 1: Drop kentucky_counties if it already exists  in the database */
+
+drop table if exists
+		lab_10.ky_opioid; -- end first statement
+
+/* Step 2: Create ky_opioid from the results of the query of kentucky_counties and opioid_mortality */
+create table
+		lab_10.ky_opioid -- schema_name.table_name
+
+as
+select
+
+		lab_10.kentucky_counties.id,
+		lab_10.kentucky_counties.countyfp,
+		lab_10.kentucky_counties.geom,
+		lab_10.kentucky_counties.name,
+		lab_10.kentucky_counties.lsad,
+		lab_10.kentucky_counties.aland,
+		lab_10.kentucky_counties.awater,
+		lab_10.opoid_mortality.opioid_2018
+		
+from
+		lab_10.kentucky_counties
+		
+left join -- keep all counties in output
+		lab_10.opoid_mortality
+on
+		lab_10.kentucky_counties.name = lab_10.opoid_mortality.county;
+		
+alter table
+		lab_10.ky_opioid
+add primary key (id);
